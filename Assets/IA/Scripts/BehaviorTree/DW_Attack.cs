98ed5563a7b127aee5d6f384b11a198c6223f128
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2023-12-18 14:11:18 +0100

    IA attack

---
+++ Assets/IA/Scripts/BehaviorTree/DW_Attack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Node_script;
+
+public class DW_Attack : Node
+{
+    // get la class actuel du joueur et lui appliquer des dégats (apres chaque coups nodestate = failure pendant x temps
+    private GameObject Player;
+    private GameObject Enemy;
+    private float time_between_attacks;
+    private float last_attack_time;
+
+
+    public DW_Attack(GameObject enemy, GameObject player, float timeBetweenAttacks)
+    {
+        Enemy = enemy;
+        Player = player;
+        time_between_attacks = timeBetweenAttacks;
+    }  
+
+    public override NodeState Evaluate()
+    {
+        if (Time.time - last_attack_time >= time_between_attacks)
+        {
+            Player.GetComponent<DW_LifeManager>().TakeDamage(Enemy.GetComponent<DW_LifeManager>().damage);
+
+            last_attack_time = Time.time;
+
+            Debug.Log("Attack Player");
+            return NodeState.SUCCESS;
+        }
+        else
+        {
+            return NodeState.RUNNING;
+        }
+    }
+
+
+}
