5bd3f55a4c07994c360860393bbc09b9383e49b1
matcodeew <150682957+matcodeew@users.noreply.github.com>
2023-12-21 11:50:43 +0100

    DEV
    place item
    drop scale

--- Assets/Inventory/Scripts/DW_DropController.cs
+++ Assets/Inventory/Scripts/DW_DropController.cs
@@ -8,6 +8,7 @@ public class DW_DropController : MonoBehaviour
 
     [SerializeField] private List<GameObject> m_Object;
     [SerializeField] DW_Character player_pos;
+    [SerializeField] private Transform mapforest, mapHospital;
 
     private GameObject current_gameobject;
 
@@ -26,8 +27,18 @@ public class DW_DropController : MonoBehaviour
             if (m_Object[i].GetComponent<DW_Item>().GetItem() == item)
             {
                 current_gameobject = Instantiate<GameObject>(m_Object[i]);
-                current_gameobject.transform.position = position;
+                current_gameobject.transform.localPosition = new Vector3(position.x,0,position.z);
                 current_gameobject.GetComponent<DW_Item>().m_Position = new Vector2(Mathf.Abs(position.x / 10), Mathf.Abs(position.z / 10));
+                current_gameobject.transform.localScale = new Vector3(5,5,5);
+                if(mapforest.gameObject.activeSelf)
+                {
+                    current_gameobject.transform.parent = mapforest;
+                }
+                else
+                {
+                    current_gameobject.transform.parent = mapHospital;
+                }
+                current_gameobject.SetActive(true);
                 DW_ObjectDetection.Instance.AddObject(current_gameobject);
                 Debug.Log("Successfuly dropped");
                 return true;
