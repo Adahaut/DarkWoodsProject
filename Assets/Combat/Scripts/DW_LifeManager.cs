679439baec29760cdfed11bed70e9acf183c7b32
shenanigan11 <enzo2dessaint@gmail.com>
2023-12-21 09:50:54 +0100

    Game(B)
    - position -0.5 at first move
    - player on same box than AI
    - box don't be a 2 when the AI die

--- Assets/Combat/Scripts/DW_LifeManager.cs
+++ Assets/Combat/Scripts/DW_LifeManager.cs
@@ -59,7 +59,9 @@ public class DW_LifeManager : MonoBehaviour
         if(gameObject.tag == "Player")
         {
             DW_GM_Classes.Instance.ClassDeath(class_died);
-            this.GetComponent<DW_ClassController>().ResetAggro();
+            Vector2 Pos = gameObject.GetComponent<DW_Character>().GetPos();
+            DW_GridMap.Instance.Grid[(int)Pos.x, (int)Pos.y] = 2;
+            this.GetComponent<DW_ClassController>().ResetAggro(); 
         }
 
         //enemies death
